7ee872b80075e1a6edeec00374219096ac7c4728
4D-08\koyo <a>
2026-06-09 13:42:18 +0900

    プロジェクト ファイルを追加します。

---
+++ null 1.cpp
@@ -0,0 +1,38 @@
+﻿// null 1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <assert.h>
+
+
+class Test
+{
+public:
+		void Show()
+		{
+			std::cout << "Hello,World!" << std::endl;
+		}
+
+};
+
+void Func(Test*test)
+{
+	//こうあるべきだ
+	assert(test != nullptr);
+
+	test->Show();
+
+}
+
+int main()
+{
+	Func(new Test());
+	Func(nullptr);
+
+
+	//if (p != nullptr)
+	//{
+	//	delete p;
+	// }
+}
+
